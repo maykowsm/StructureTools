d17f1b122b8c762655db0a7a3569c16e9a610d45
Maykow Menezes <eng.maykowmenezes@gmail.com>
2025-02-22 23:32:22 -0300

    1 commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# StructureTools
+
+Workbench for 2d and 3d structural analysis
+
+## Dependencies
+
+['numpy','scipy','prettytable','PyniteFEA']
+
+## Quick Start
+
+TODO: add details here
+
+## Maintainer
+
+Maykow Menezes
+eng.maykowmenezes@gmail.com
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import os
+
+version_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            "freecad", "StructureTools", "version.py")
+with open(version_path) as fp:
+    exec(fp.read())
+
+setup(name='freecad.StructureTools',
+      version=str(__version__),
+      packages=['freecad',
+                'freecad.StructureTools'],
+      maintainer="Maykow Menezes",
+      maintainer_email="eng.maykowmenezes@gmail.com",
+      url="https://www.patreon.com/c/StructureTools",
+      description="Workbench for 2d and 3d structural analysis",
+      install_requires=[['numpy','scipy','prettytable','PyniteFEA']],
+      include_package_data=True)

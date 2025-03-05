9be72703c3ce615381a549e95e25bfeacf2a0d00
b-run <b_linhares@outlook.com>
2025-03-04 23:15:29 -0300

    feat pontual load. carga pontual nao somente limitado aos nos.

--- freecad/StructureTools/load_nodal.py
+++ freecad/StructureTools/load_nodal.py
@@ -1,5 +1,7 @@
 import FreeCAD, App, FreeCADGui, Part, os
 from PySide2 import QtWidgets
+from .utils_func import rotate_to_direction, make_arrow, set_obj_appear, BASE_ARROWS_DIM
+
 
 ICONPATH = os.path.join(os.path.dirname(__file__), "resources")
 
@@ -14,81 +16,36 @@ def show_error_message(msg):
 
 class LoadNodal:
     def __init__(self, obj, selection):
-        obj.Proxy = self
-        obj.addProperty("App::PropertyLinkSubList", "ObjectBase", "Base", "Object base")
-        obj.addProperty("App::PropertyForce", "NodalLoading", "Nodal", "Nodal loading").NodalLoading = 10000000
-        obj.addProperty("App::PropertyFloat", "ScaleDraw", "Load", "Scale from drawing").ScaleDraw = 1
-        
-        obj.addProperty("App::PropertyEnumeration", "GlobalDirection","Load","Global direction load")
-        obj.GlobalDirection = ['+X','-X', '+Y','-Y', '+Z','-Z']
-        obj.GlobalDirection = '-Z'
-        
-        print(selection)
-        obj.ObjectBase = (selection[0], selection[1])
-    
-    # Desenha carregamento pontual
-    def drawNodeLoad(self, obj, vertex):
-        pass
-    
-    # Retorna o subelemento asociado
-    def getSubelement(self, obj, nameSubElement):
-        
-        if 'Edge' in  nameSubElement:
-            index = int(nameSubElement.split('Edge')[1]) - 1
-            return obj.ObjectBase[0][0].Shape.Edges[index]
-        else:
-            index = int(nameSubElement.split('Vertex')[1]) - 1
-            return obj.ObjectBase[0][0].Shape.Vertexes[index]
+        super().__init__(obj, selection)
+        obj.addProperty("App::PropertyForce", "NodalLoading", "Pontual", "Nodal loading").NodalLoading = 0
+        obj.addProperty("App::PropertyLength", "NodalLoadingAt", "Pontual", "Nodal loading At (distance from start)").NodalLoadingAt = 0
 
-    # Desenha a forma da seta levando em conta a escala informada
-    def makeArrow(self, obj, load):
-        radiusCone = 5
-        heightCone = 20
-        heightCylinder = 30
-        radiusCylinder = 2
-
-        cone = Part.makeCone(0 ,radiusCone * obj.ScaleDraw * load/1000000, heightCone * obj.ScaleDraw * load/1000000)
-        cylinder = Part.makeCylinder(radiusCylinder * obj.ScaleDraw * load/1000000, heightCylinder * obj.ScaleDraw * load/1000000)        
-        cylinder.translate(FreeCAD.Vector(0,0, heightCone * obj.ScaleDraw * load/1000000))
-        return Part.makeCompound([cone, cylinder])
-    
-    
     def execute(self, obj):        
         subelement = self.getSubelement(obj, obj.ObjectBase[0][1][0])
-        if 'Vertex' in obj.ObjectBase[0][1][0]:
-            # Desenha carregamento pontual 
-            shape = self.makeArrow(obj, obj.NodalLoading)
-            
-            match obj.GlobalDirection:
-                case '+X':
-                    shape.rotate(FreeCAD.Vector(0,0,0),FreeCAD.Vector(0,1,0), -90)
-                case '-X':
-                    shape.rotate(FreeCAD.Vector(0,0,0),FreeCAD.Vector(0,1,0), 90)
-                case '+Y':
-                    shape.rotate(FreeCAD.Vector(0,0,0),FreeCAD.Vector(1,0,0), 90)
-                case '-Y':
-                    shape.rotate(FreeCAD.Vector(0,0,0),FreeCAD.Vector(1,0,0), -90)
-                case '+Z':
-                    shape.rotate(FreeCAD.Vector(0,0,0),FreeCAD.Vector(1,0,0), 180)
-                case '-Z':
-                    shape.rotate(FreeCAD.Vector(0,0,0),FreeCAD.Vector(1,0,0), 0)
-            
-            
-            shape.translate(subelement.Point)
-            obj.ViewObject.ShapeAppearance = (FreeCAD.Material(DiffuseColor=(1.00,0.00,0.00),AmbientColor=(0.33,0.33,0.33),SpecularColor=(0.53,0.53,0.53),EmissiveColor=(0.00,0.00,0.00),Shininess=(0.90),Transparency=(0.00),))
-            obj.Label = 'nodal load'
+        if obj.NodalLoading == 0:
+            obj.NodalLoading = self.base_value
+
+        shape = make_arrow(obj.NodalLoading,**BASE_ARROWS_DIM, scale=obj.ScaleDraw)
+        rotate_to_direction(obj.GlobalDirection, shape)
+        shape.translate(subelement.Vertexes[0].Point)
+        set_obj_appear(obj,1)
+        
+        if 'Edge' in obj.ObjectBase[0][1][0]:
+            if obj.NodalLoadingAt > subelement.Length:
+                obj.NodalLoadingAt = 0
+
+            for coordinades in self.get_arrow_coordinades(0,subelement, obj.NodalLoadingAt):
+
+                shape.translate(coordinades)
+                obj.Label = 'Pontual load'
+
+        else:
+            obj.NodalLoadingAt = 0
+            obj.Label = 'Nodal load'
 
         obj.Placement = shape.Placement
         obj.Shape = shape
         obj.ViewObject.DisplayMode = 'Shaded'
-        
-        
-       
-
-
-    def onChanged(self,obj,Parameter):
-        if Parameter == 'edgeLength':
-            self.execute(obj)
     
 
 class ViewProviderLoadNodal:
